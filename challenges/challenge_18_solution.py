ef0be75191be4bd81e39e250c4145da4025970fe
jianxing-ma <jianxing.james.ma@outlook.com>
2023-07-10 22:04:55 +0000

    print exercise

---
+++ challenges/challenge_18_solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 18 - Solution"""  
+
+def main():
+    # ask user to input name
+    input_name = input("Please enter your name: ")
+
+    # ask user to input day
+    input_day = input("Please enter the day: ")
+
+    # print message
+    print("Hello, "+input_name+"! Happy "+input_day+"!")
+
+if __name__=="__main__":
+    main()
